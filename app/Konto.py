31c95787eb0bfc233fcee2d279b1490e654f9f3a
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-10-25 20:58:50 +0200

    Feature 3 znowu

--- app/Konto.py
+++ app/Konto.py
@@ -3,6 +3,11 @@ class Konto:
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
-        self.pesel=pesel
+        self.check_pesel(pesel)
+
+    def check_pesel(self,pesel):
         if(len(pesel)!=11):
             self.pesel="Niepoprawny pesel!"
+        else:
+            self.pesel=pesel
+    
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -9,6 +9,19 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.pesel, "12345678999", "Niepoprawny pesel!")
+
+
+        self.assertEqual(pierwsze_konto.pesel, "12345678999", "Pesel nie został zapisany!")
+
+        #pesel
+
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Nieprawidłowa długość peselu")
+
+        drugie_konto= Konto("Dariuszdwa", "Januszewskidwa", "12345678999")
+        self.assertEqual(len(drugie_konto.pesel), "Niepoprawny pesel!", "Pesel jest poprawny!")
+
+        #kod
+
+
 
     #tutaj proszę dodawać nowe testy
