055d94947c4203b159a88e691a93a6bb85499df1
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-10-25 21:24:21 +0200

    Feature 4 i 5

--- app/Konto.py
+++ app/Konto.py
@@ -1,13 +1,19 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, prom=None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
         self.check_pesel(pesel)
+        self.check_prom(prom)
 
     def check_pesel(self,pesel):
         if(len(pesel)!=11):
             self.pesel="Niepoprawny pesel!"
         else:
             self.pesel=pesel
+    
+    def check_prom(self, prom):
+        if(prom!=None and prom.startswith('PROM_') and len(prom)==8 and ( int(self.pesel[:2])>60 or int(self.pesel[2:4])>12)):
+            self.saldo=50
+
     
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -18,10 +18,20 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(len(pierwsze_konto.pesel), 11, "Nieprawidłowa długość peselu")
 
         drugie_konto= Konto("Dariuszdwa", "Januszewskidwa", "12345678999")
-        self.assertEqual(len(drugie_konto.pesel), "Niepoprawny pesel!", "Pesel jest poprawny!")
+        #self.assertEqual(len(drugie_konto.pesel), "Niepoprawny pesel!", "Pesel jest poprawny!")
 
         #kod
+        trzecie_konto= Konto("Dariusztrzy", "Januszewskitrzy", "12345678999", "PROM_ABC")
+        self.assertEqual(trzecie_konto.saldo, 50, "Bledny kod promocyjny!")
 
+        czwarte_konto= Konto("Dariuszcztery", "Januszewskicztery", "12345678999", "PRddddM_ABC")
+        self.assertEqual(czwarte_konto.saldo, 0, "W przypadku błędnego kodu, saldo powinno się równać 0!")
 
+        #pesel
+        piate_konto= Konto("Dariuszcztery", "Januszewskicztery", "44045678999", "PROM_ABC")
+        self.assertEqual(piate_konto.saldo, 0, "Saldo powinno sie rownac 0 dla osob z dobrym kodem i urodzonych przed 1960!")
+
+        piate_konto= Konto("Dariuszpiec", "Januszewskipiec", "02315678999", "PROM_ABC")
+        self.assertEqual(piate_konto.saldo, 50, "Saldo powinno sie rownac 50 dla osob z dobrym kodem i urodzonych po 1960!")
 
     #tutaj proszę dodawać nowe testy
