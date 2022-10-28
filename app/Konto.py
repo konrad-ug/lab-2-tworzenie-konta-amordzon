9b1a357711308c17552dd11577e554f814304a9e
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-10-28 14:47:30 +0200

    Feature 6

--- app/Konto.py
+++ app/Konto.py
@@ -1,8 +1,8 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel, prom=None):
+    def __init__(self, imie, nazwisko, pesel, saldo, prom=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
+        self.saldo = saldo
         self.check_pesel(pesel)
         self.check_prom(prom)
 
@@ -15,5 +15,11 @@ class Konto:
     def check_prom(self, prom):
         if(prom!=None and prom.startswith('PROM_') and len(prom)==8 and ( int(self.pesel[:2])>60 or (int(self.pesel[2:4])>20 and int(self.pesel[2:4])<33))):
             self.saldo=50
+    
+    def zaksieguj_przelew_wychodzacy(self,ile):
+        if(self.saldo-ile>=0):
+            self.saldo-=ile
 
+    def zaksieguj_przelew_przychodzacy(self, ile):
+        self.saldo+=ile
     
