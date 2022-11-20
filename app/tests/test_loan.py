fc85359f24b036fdb238e7b9eeaafb85d394a48e
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-11-20 15:21:17 +0100

    poprawki

--- app/tests/test_loan.py
+++ app/tests/test_loan.py
@@ -1,34 +1,24 @@
 import unittest
 
 from ..KontoOsobiste import KontoOsobiste
+from parameterized import parameterized
 
 class TestLoan(unittest.TestCase):
-    def test_loan_dobrze(self):
-        konto=KontoOsobiste("Dariusz", "Januszewski", "12345678999")
-        konto.saldo=1100
-        konto.historia=[100,200,300,400,100]
-        self.assertEqual(konto.zaciagnij_kredyt(200), True, "Nie udało się zaciágnąć kredytu!")
-        self.assertEqual(konto.saldo, 1300, "Niepoprawna kwota!")
+    def setUp(self):
+        self.konto=KontoOsobiste("Dariusz", "Januszewski", "12345678999")
+
+    @parameterized.expand([
+        ([100,200,300,400,100], 100, True, 100),
+        ([100,200], 100, False, 0),
+        ([-100,200,300,400,100], 100, False, 0),
+        ([100,200,300,400,100], 3000, False, 0),
+        ([100,200,300,400,100,-600], 300, True, 300),
+    ])
+
+    def test_loan(self, historia, kwota, oczek_wyn, oczek_saldo):
+        self.konto.historia=historia
+        self.assertEqual(self.konto.zaciagnij_kredyt(kwota), oczek_wyn, "Nie udało się zaciágnąć kredytu!")
+        self.assertEqual(self.konto.saldo, oczek_saldo, "Niepoprawna kwota!")
     
-    def test_loan_za_malo_przelewow(self):
-        konto=KontoOsobiste("Dariusz", "Januszewski", "12345678999")
-        konto.saldo=1100
-        konto.historia=[100,200]
-        self.assertEqual(konto.zaciagnij_kredyt(200), False, "W przypadku za malej ilosci przelewow powinien być błąd!")
-        self.assertEqual(konto.saldo, 1100, "Niepoprawna kwota!")
-    
-    def test_loan_przelew_wyplata(self):
-        konto=KontoOsobiste("Dariusz", "Januszewski", "12345678999")
-        konto.saldo=1100
-        konto.historia=[-100,200,300,400,100]
-        self.assertEqual(konto.zaciagnij_kredyt(200), False, "W przypadku wyplaty kredyt nie powinien byc moc zaciagniety!")
-        self.assertEqual(konto.saldo, 1100, "Niepoprawna kwota!")
-    
-    def test_loan_za_duzy_kredyt(self):
-        konto=KontoOsobiste("Dariusz", "Januszewski", "12345678999")
-        konto.saldo=1100
-        konto.historia=[100,200,300,400,100]
-        self.assertEqual(konto.zaciagnij_kredyt(20000), False,  "W przypadku kiedy kredyt jest za duzy powinien byc blad!")
-        self.assertEqual(konto.saldo, 1100, "Niepoprawna kwota!")
 
 
